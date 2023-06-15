3d8f38d1ea27153727ecae67a9f3dcd99adfc67c
NiceShadow <niceshadowhd@gmail.com>
2023-06-15 12:45:43 +0200

    localization test

---
+++ Assets/MemberFolder/Lewin/MultiLanguage.cs
@@ -0,0 +1,16 @@
+using Assets.SimpleLocalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiLanguage : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        LocalizationManager.Read();
+
+        LocalizationManager.Language = "English";
+    }
+
+}
